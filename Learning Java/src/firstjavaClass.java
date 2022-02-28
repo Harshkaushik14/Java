678014b2eecde1d122ef4333649fe93d8e649e0f
Harsh Kaushik <55058037+Harshkaushik14@users.noreply.github.com>
2022-02-28 11:43:22 +0530

    java

---
+++ Learning Java/src/firstjavaClass.java
@@ -0,0 +1,10 @@
+
+public class firstjavaClass {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		int x= 10 , y = 20 , z=30;
+		System.out.println(x+y+z);
+	}
+
+}
